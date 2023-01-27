f7a4bbc0b284250bd44635f4920ff9b6012512ef
antonsemenov8703 <102480900+antonsemenov8703@users.noreply.github.com>
2023-01-27 22:43:39 +0530

    Seminar2

---
+++ src/Seminars/Task02_1.java
@@ -0,0 +1,32 @@
+package Seminars;
+
+public class Task02_1 {
+
+    // Посчитайте сколько драгоценных камней в куче обычных камней
+    // Пример:
+    // jewels = “aB”, stones = “aaaAbbbB”
+    // Результат в консоль ”a3B1”
+    public static void main(String[] args) {
+        String jewels = "aB";
+        String stones = "aaaAbbbB";
+
+        System.out.println(findJewelsInStones(jewels, stones));
+    }
+
+    public static String findJewelsInStones(String jewels, String stones) {
+        String rez = "";
+
+        for(int i = 0; i < jewels.length(); i++) {
+            int count = 0;
+            for(int j = 0; j < stones.length(); j++) {
+                if(jewels.charAt(i) == stones.charAt(j)) {
+                    count++;
+                }
+            }
+            rez = rez + jewels.charAt(i) + count;
+            count = 0;
+        }
+        return rez;
+    }
+
+}
